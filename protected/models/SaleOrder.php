c99cec8bec43c8f55fcf806b855fe4791dac17d3
noonnightcoder <noonnightcoder@gmail.com>
2015-06-23 06:36:24 +0700

    fine tune sql

--- protected/models/SaleOrder.php
+++ protected/models/SaleOrder.php
@@ -99,46 +99,6 @@ class SaleOrder extends CActiveRecord
     }
 
     /**
-     * Retrieves a list of models based on the current search/filter conditions.
-     *
-     * Typical usecase:
-     * - Initialize the model fields with values from filter form.
-     * - Execute this method to get CActiveDataProvider instance which will filter
-     * models according to data in model fields.
-     * - Pass data provider to CGridView, CListView or any similar widget.
-     *
-     * @return CActiveDataProvider the data provider that can return the models
-     * based on the search/filter conditions.
-     */
-    public function search()
-    {
-        // @todo Please modify the following code to remove attributes that should not be searched.
-
-        $criteria = new CDbCriteria;
-
-        $criteria->compare('id', $this->id);
-        $criteria->compare('sale_time', $this->sale_time, true);
-        $criteria->compare('client_id', $this->client_id);
-        $criteria->compare('desk_id', $this->desk_id);
-        $criteria->compare('zone_id', $this->zone_id);
-        $criteria->compare('group_id', $this->group_id);
-        $criteria->compare('employee_id', $this->employee_id);
-        $criteria->compare('location_id', $this->location_id);
-        $criteria->compare('sub_total', $this->sub_total);
-        $criteria->compare('payment_type', $this->payment_type, true);
-        $criteria->compare('status', $this->status, true);
-        $criteria->compare('remark', $this->remark, true);
-        $criteria->compare('discount_amount', $this->discount_amount, true);
-        $criteria->compare('discount_type', $this->discount_type, true);
-        $criteria->compare('giftcard_id', $this->giftcard_id);
-        $criteria->compare('empty_flag', $this->empty_flag);
-
-        return new CActiveDataProvider($this, array(
-            'criteria' => $criteria,
-        ));
-    }
-
-    /**
      * Returns the static model of the specified AR class.
      * Please note that you should have this exact method in all your CActiveRecord descendants!
      * @param string $className active record class name.
@@ -151,21 +111,14 @@ class SaleOrder extends CActiveRecord
 
     public function getOrderCart($desk_id, $group_id, $location_id)
     {
-        $sql = "SELECT item_number,item_id,`name`,quantity,price,discount,
+        $sql = "SELECT item_number,item_id,`name`,quantity,price,discount_amount discount,
                 total,client_id,desk_id,zone_id,employee_id,qty_in_stock,topping,item_parent_id,category_id
-                FROM (
-                SELECT item_number,item_id,`name`,quantity,price,discount_amount discount,
-                CASE WHEN discount_type='%' THEN (price*quantity-price*quantity*discount_amount/100) 
-                     ELSE price*quantity-discount_amount
-                END total,
-                client_id,desk_id,zone_id,employee_id,qty_in_stock,topping,item_parent_id,category_id
-                FROM v_order_cart 
+                FROM v_order_cart
                 WHERE desk_id=:desk_id 
                 AND group_id=:group_id 
                 AND location_id=:location_id
                 AND status=:status
-                ORDER BY path,modified_date
-                ) AS t1";
+                ORDER BY path,modified_date";
 
         return Yii::app()->db->createCommand($sql)->queryAll(true, array(
                 ':desk_id' => $desk_id,
@@ -176,6 +129,106 @@ class SaleOrder extends CActiveRecord
         );
     }
 
+    public function getOrderToKitchen($desk_id, $group_id, $location_id, $category_id)
+    {
+        $sql = "SELECT item_number,item_id,`name`,quantity,price,discount,
+                total,client_id,desk_id,zone_id,employee_id,qty_in_stock,topping,item_parent_id
+                FROM (
+                SELECT t1.item_number,t1.item_id,t1.`name`,(t1.quantity-IFNULL(t2.`quantity`,0)) quantity,t1.price,t1.discount_amount discount,
+                t1.total,
+                t1.client_id,t1.desk_id,t1.zone_id,t1.employee_id,t1.qty_in_stock,t1.topping,t1.item_parent_id
+                FROM v_order_cart t1 LEFT JOIN
+                        (SELECT t2.sale_id,t2.item_id,t2.item_parent_id ,t2.quantity
+                         FROM sale_order_item_print t2 , item t3
+                         WHERE t3.id=t2.item_id
+                         AND t3.category_id=:category_id
+                        ) t2
+                    ON t2.sale_id=t1.`sale_id`
+                    AND t2.item_id=t1.item_id
+                    AND t2.item_parent_id=t1.item_parent_id
+                WHERE t1.desk_id=:desk_id AND t1.group_id=:group_id and t1.location_id=:location_id
+                AND t1.status=:status
+                AND (t1.quantity-IFNULL(t2.quantity,0))>0
+                AND t1.category_id=:category_id
+                ORDER BY t1.path,t1.modified_date
+                ) AS t1";
+
+        return Yii::app()->db->createCommand($sql)->queryAll(true, array(
+                ':desk_id' => $desk_id,
+                ':group_id' => $group_id,
+                ':location_id' => $location_id,
+                ':category_id' => $category_id,
+                ':status' => $this->active_status
+            )
+        );
+    }
+
+    public function getOrderCartTopping($desk_id, $group_id, $location_id)
+    {
+        $sql = "SELECT item_id,`name`,quantity,price,discount_amount discount,total,
+                client_id,desk_id,zone_id,employee_id,qty_in_stock
+                FROM v_order_cart
+                WHERE desk_id=:desk_id AND group_id=:group_id and location=:location_id
+                AND status=:status
+                AND topping=1
+                ORDER BY modified_date desc";
+
+
+        return Yii::app()->db->createCommand($sql)->queryAll(true, array(
+            ':desk_id' => (int)$desk_id,
+            ':group_id' => $group_id,
+            ':location_id' => $location_id,
+            ':status' => $this->active_status
+        ));
+    }
+
+    public function getAllTotal($desk_id, $group_id, $location_id)
+    {
+        $quantity = 0;
+        $sub_total = 0;
+        $total = 0;
+        $discount_amount = 0;
+
+        $sql = "SELECT sale_id,sum(quantity) quantity,
+                    SUM(price*quantity) sub_total,
+                    SUM(price*quantity) - (SUM(price*quantity)*IFNULL(so.discount_amount,0)/100) total,
+                    SUM(price*quantity)*IFNULL(so.discount_amount,0)/100 discount_amount
+                FROM v_order_cart oc JOIN sale_order so
+                            ON so.id=oc.sale_id
+                            and so.desk_id=oc.desk_id
+                            and so.group_id=oc.group_id
+                            and so.location_id=oc.location_id
+                WHERE oc.desk_id=:desk_id
+                AND oc.group_id=:group_id
+                AND oc.location_id=:location_id
+                and oc.status=:status
+                GROUP BY sale_id";
+
+        $sql = "SELECT s.id sale_id,SUM(so.quantity) quantity,SUM(so.total) sub_total,sum(total) - sum(total)*IFNULL(s.discount_amount,0)/100 total,sum(total)*IFNULL(s.discount_amount,0)/100 discount_amount
+                FROM sale_order s JOIN v_sale_order_tem_sum so ON so.sale_id = s.id
+                WHERE s.desk_id=:desk_id
+                AND s.group_id=:group_id
+                AND s.location_id=:location_id
+                AND s.status=:status
+                GROUP BY s.id";
+
+        $result = Yii::app()->db->createCommand($sql)->queryAll(true, array(
+            ':desk_id' => $desk_id,
+            ':group_id' => $group_id,
+            ':location_id' => $location_id,
+            ':status' => $this->active_status
+        ));
+        if ($result) {
+            foreach ($result as $record) {
+                $quantity = $record['quantity'];
+                $sub_total = $record['sub_total'];
+                $total = $record['total'];
+                $discount_amount = $record['discount_amount'];
+            }
+        }
+
+        return array($quantity, $sub_total, $total, $discount_amount);
+    }
 
     /*
      * To save ordering item replacing saveItem using PHP session
@@ -209,42 +262,6 @@ class SaleOrder extends CActiveRecord
         );
     }
 
-    public function getOrderToKitchen($desk_id, $group_id, $location_id, $category_id)
-    {
-        $sql = "SELECT item_number,item_id,`name`,quantity,price,discount,
-                total,client_id,desk_id,zone_id,employee_id,qty_in_stock,topping,item_parent_id
-                FROM (
-                SELECT t1.item_number,t1.item_id,t1.`name`,(t1.quantity-IFNULL(t2.`quantity`,0)) quantity,t1.price,t1.discount_amount discount,
-                CASE WHEN t1.discount_type='%' THEN (t1.price*t1.quantity-t1.price*t1.quantity*t1.discount_amount/100) 
-                     ELSE t1.price*t1.quantity-t1.discount_amount
-                END total,
-                t1.client_id,t1.desk_id,t1.zone_id,t1.employee_id,t1.qty_in_stock,t1.topping,t1.item_parent_id
-                FROM v_order_cart t1 LEFT JOIN 
-                        (SELECT t2.sale_id,t2.item_id,t2.item_parent_id ,t2.quantity
-                         FROM sale_order_item_print t2 , item t3
-                         WHERE t3.id=t2.item_id
-                         AND t3.category_id=:category_id
-                        ) t2
-                    ON t2.sale_id=t1.`sale_id`
-                    AND t2.item_id=t1.item_id
-                    AND t2.item_parent_id=t1.item_parent_id
-                WHERE t1.desk_id=:desk_id AND t1.group_id=:group_id and t1.location_id=:location_id
-                AND t1.status=:status
-                AND (t1.quantity-IFNULL(t2.quantity,0))>0 
-                AND t1.category_id=:category_id
-                ORDER BY t1.path,t1.modified_date
-                ) AS t1";
-
-        return Yii::app()->db->createCommand($sql)->queryAll(true, array(
-                ':desk_id' => $desk_id,
-                ':group_id' => $group_id,
-                ':location_id' => $location_id,
-                ':category_id' => $category_id,
-                ':status' => $this->active_status
-            )
-        );
-    }
-
     public function savePrintedToKitchen($desk_id, $group_id, $location_id, $category_id)
     {
         $sql = "CALL pro_save_pkitchen(:desk_id,:group_id,:location_id,:category_id)";
@@ -257,167 +274,6 @@ class SaleOrder extends CActiveRecord
             )
         );
     }
-
-
-    public function getOrderCartTopping($desk_id, $group_id, $location_id)
-    {
-        $sql = "SELECT item_id,`name`,quantity,price,discount_amount discount,
-                CASE WHEN discount_type='%' THEN (price*quantity-price*quantity*discount_amount/100) 
-                     ELSE price*quantity-discount_amount
-                END total,
-                client_id,desk_id,zone_id,employee_id,qty_in_stock
-                FROM v_order_cart 
-                WHERE desk_id=:desk_id AND group_id=:group_id and location=:location_id
-                AND status=:status
-                AND topping=1
-                ORDER BY modified_date desc";
-
-
-        return Yii::app()->db->createCommand($sql)->queryAll(true, array(
-            ':desk_id' => (int)$desk_id,
-            ':group_id' => $group_id,
-            ':location_id' => $location_id,
-            ':status' => $this->active_status
-        ));
-    }
-
-    public function getAllTotal($desk_id, $group_id, $location_id)
-    {
-        $quantity = 0;
-        $sub_total = 0;
-        $total = 0;
-        $discount_amount = 0;
-
-        $sql = "SELECT sale_id,sum(quantity) quantity,
-                    SUM(price*quantity) sub_total,
-                    SUM(price*quantity) - (SUM(price*quantity)*IFNULL(so.discount_amount,0)/100) total,
-                    SUM(price*quantity)*IFNULL(so.discount_amount,0)/100 discount_amount
-                FROM v_order_cart oc JOIN sale_order so
-                            ON so.id=oc.sale_id 
-                            and so.desk_id=oc.desk_id
-                            and so.group_id=oc.group_id
-                            and so.location_id=oc.location_id
-                WHERE oc.desk_id=:desk_id 
-                AND oc.group_id=:group_id 
-                AND oc.location_id=:location_id
-                and oc.status=:status
-                GROUP BY sale_id";
-
-        $result = Yii::app()->db->createCommand($sql)->queryAll(true, array(
-            ':desk_id' => $desk_id,
-            ':group_id' => $group_id,
-            ':location_id' => $location_id,
-            ':status' => $this->active_status
-        ));
-        if ($result) {
-            foreach ($result as $record) {
-                $quantity = $record['quantity'];
-                $sub_total = $record['sub_total'];
-                $total = $record['total'];
-                $discount_amount = $record['discount_amount'];
-            }
-        }
-
-        return array($quantity, $sub_total, $total, $discount_amount);
-    }
-
-    /*
-    public function getOrderTotalQty($desk_id,$group_id,$location_id)
-    {
-         $sql="SELECT SUM(quantity) quantity
-               FROM v_order_cart
-               WHERE desk_id=:desk_id
-               AND group_id=:group_id
-               AND location_id=:location_id
-               AND status=:status";
-
-        $result=Yii::app()->db->createCommand($sql)->queryAll(true, array(':desk_id' => (int) $desk_id, ':group_id'=>$group_id, ':location_id' => (int) $location_id,':status'=>$this->active_status));
-
-        foreach ($result as $record) {
-            $quantity = $record['quantity'];
-        }
-
-        return $quantity;
-    }
-
-    public function getOrderSubTotal($desk_id,$group_id, $location_id)
-    {
-        $sql="SELECT sale_id,SUM(price*quantity) sub_total,
-            SUM(price*quantity) - (SUM(price*quantity)*IFNULL(so.discount_amount,0)/100) total
-            FROM v_order_cart oc JOIN sale_order so
-                    ON so.id=oc.sale_id
-            WHERE so.desk_id=:desk_id
-            AND so.group_id=:group_id
-            AND so.location_id=:location_id
-            and oc.status=:status
-            GROUP BY sale_id";
-
-        $result=Yii::app()->db->createCommand($sql)->queryAll(true, array(':desk_id' => (int) $desk_id, ':group_id'=>$group_id, ':location_id' => (int) $location_id,':status'=>$this->active_status));
-
-        if ($result) {
-            foreach ($result as $record) {
-                $total = $record['sub_total'];
-            }
-        } else {
-            $total=0;
-        }
-
-        return $total;
-    }
-
-    public function getOrderTotal($desk_id,$group_id,$location_id)
-    {
-
-        $total=0;
-
-        $sql="
-            SELECT sale_id,sub_total,
-                CEIL(total/100)*100 total
-            FROM (
-                SELECT sale_id,SUM(price*quantity) sub_total,
-                SUM(price*quantity) - (SUM(price*quantity)*IFNULL(so.discount_amount,0)/100) total
-                FROM v_order_cart oc JOIN sale_order so
-                        ON so.id=oc.sale_id
-                WHERE so.desk_id=:desk_id
-                and so.group_id=:group_id
-                and so.location_id=:location_id
-                and oc.status=:status
-                GROUP BY sale_id
-            ) as t1";
-
-        $result=Yii::app()->db->createCommand($sql)->queryAll(true, array(':desk_id' => (int) $desk_id, ':group_id'=>$group_id, ':location_id' => (int) $location_id,':status'=>$this->active_status));
-
-        if ($result) {
-            foreach ($result as $record) {
-                $total = $record['total'];
-            }
-        }
-
-        return $total;
-    }
-
-    public function getOrderDiscount($desk_id,$group_id,$location_id)
-    {
-        $total=0;
-
-        $sql="SELECT sale_id,
-             SUM(price*quantity)*IFNULL(so.discount_amount,0)/100 discount_amount
-            FROM v_order_cart oc JOIN sale_order so
-                    ON so.id=oc.sale_id
-            WHERE so.desk_id=:desk_id and so.group_id=:group_id
-            and so.location_id=:location_id
-            and oc.status=:status
-            GROUP BY sale_id";
-
-        $result=Yii::app()->db->createCommand($sql)->queryAll(true, array(':desk_id' => (int) $desk_id, ':group_id'=>$group_id, ':location_id' => (int) $location_id,':status'=>$this->active_status));
-
-        foreach ($result as $record) {
-            $total = $record['discount_amount'];
-        }
-        return $total;
-    }
-     *
-    */
 
     public function delOrderItem($item_id, $item_parent_id, $desk_id, $group_id, $location_id)
     {
